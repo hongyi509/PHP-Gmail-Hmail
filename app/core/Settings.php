06dd9a88d4d2dac299c87f1df21ec283c059f2d3
ReactDev313 <reactist313@hotmail.com>
2021-03-09 07:14:58 -0600

    update setting

--- app/core/Settings.php
+++ app/core/Settings.php
@@ -2,19 +2,20 @@
 
 function getBaseUrl()
 {
-    $use_forwarded_host = false;
-    $ssl = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on');
-    $server_protocol = strtolower($_SERVER['SERVER_PROTOCOL']);
-    $protocol = substr($server_protocol, 0, strpos($server_protocol, '/')) . (($ssl) ? 's' : '');
-    $port = $_SERVER['SERVER_PORT'];
-    $port = ((!$ssl && $port == '80') || ($ssl && $port == '443')) ? '' : ':' . $port;
-    $host = ($use_forwarded_host && isset($_SERVER['HTTP_X_FORWARDED_HOST'])) ? $_SERVER['HTTP_X_FORWARDED_HOST'] : (isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : null);
-    $host = isset($host) ? $host : $_SERVER['SERVER_NAME'] . $port;
-    return $protocol . '://' . $host;
+    // $use_forwarded_host = false;
+    // $ssl = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on');
+    // $server_protocol = strtolower($_SERVER['SERVER_PROTOCOL']);
+    // $protocol = substr($server_protocol, 0, strpos($server_protocol, '/')) . (($ssl) ? 's' : '');
+    // $port = $_SERVER['SERVER_PORT'];
+    // $port = ((!$ssl && $port == '80') || ($ssl && $port == '443')) ? '' : ':' . $port;
+    // $host = ($use_forwarded_host && isset($_SERVER['HTTP_X_FORWARDED_HOST'])) ? $_SERVER['HTTP_X_FORWARDED_HOST'] : (isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : null);
+    // $host = isset($host) ? $host : $_SERVER['SERVER_NAME'] . $port;
+    // return $protocol . '://' . $host;
+    return 'https://thawing-headland-68770.herokuapp.com';
 }
 
-// $BASE_URL = getBaseUrl();
-$BASE_URL = 'https://thawing-headland-68770.herokuapp.com';
+$BASE_URL = getBaseUrl();
+// $BASE_URL = 'https://thawing-headland-68770.herokuapp.com';
 /* Google App Client Id */
 define('CLIENT_ID', '233119568975-l0t6n9934cqq9tiulkpd1jk6hu0trs4s.apps.googleusercontent.com');
 
