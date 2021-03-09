46cb29283bf17011f23e542caf566cf3d269a6fc
ReactDev313 <reactist313@hotmail.com>
2021-03-09 09:20:08 -0600

    update debeug home

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -11,16 +11,18 @@ class Home extends Controller
                 $auth_service->get_token($_GET['code']);
                 // Save the access token as a session variable
                 // Redirect to the page where user can create event
-                echo('index home');
-                header('Location: ./dashboard/index');
+                // echo('index home');
+                // header('Location: ./dashboard/index');
+                 return parent::view('dashboard/index');
                 die();
             } catch (Exception $e) {
                 echo $e->getMessage();
             }
         }
         if (isset($_SESSION['access_token']) && isset($_COOKIE['refresh_token'])) {
-            header('Location: ./dashboard/index');
-            echo($_SERVER['HTTP_HOST'].'---'.dirname($_SERVER['PHP_SELF']));
+            return parent::view('dashboard/index');
+            // header('Location: ./dashboard/index');
+            // echo($_SERVER['HTTP_HOST'].'---'.dirname($_SERVER['PHP_SELF']));
         }
             // $home_url = 'https://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) ;
  echo($_SERVER['HTTP_HOST'].'---'.dirname($_SERVER['PHP_SELF']));
